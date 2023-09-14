138ba82736bc68a39e27d3cbbe0f65f5fd984096
julideviaje <hola.julideviaje@gmail.com>
2023-09-14 15:09:13 -0300

    fix deploy

--- app/core/config.py
+++ app/core/config.py
@@ -1,17 +1,12 @@
+import os
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    MONGO_DB_NAME: str
-    DB_URI: str
-    SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
-
-    class Config:
-        case_sensitive = True
-        env_file = ".env"
-        env_file_encoding = "utf-8"
-
+    MONGO_DB_NAME: str = os.getenv("MONGO_DB_NAME")
+    DB_URI: str = os.getenv("DB_URI")
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    ALGORITHM: str = os.getenv("ALGORITHM")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 
 settings = Settings()
